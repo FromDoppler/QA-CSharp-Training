d3f5955350a4c81f78887eb4fc454ff07132a08d
CarlosSampedro <csampedro@makingsense.com>
2020-09-18 21:44:23 -0300

    List_example(console_app)

--- CSharp_Programming_Training_Complete_Course/CSharp_Programming_Training/Program.cs
+++ CSharp_Programming_Training_Complete_Course/CSharp_Programming_Training/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace QA_CSharp_Training
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> employees = new List<Employee> 
+            {
+                new Employee {Name ="Roxanne" },
+                new Employee {Name ="Alex" }
+            };
+
+
+            employees.Add(new Employee ("Dani"));
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+
+            for (int i =0; i< employees.Count; i++)
+            {
+                Console.WriteLine(employees[i].Name);
+            }
         }
     }
 }
