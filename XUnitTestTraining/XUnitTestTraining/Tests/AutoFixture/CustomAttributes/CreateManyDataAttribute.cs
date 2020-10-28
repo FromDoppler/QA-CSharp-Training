326c53eecedd6271d43541439b13779ac0a19af1
María Hansen <mhansen@makingsense.com>
2020-10-28 13:57:44 -0300

    XUnitTestTraining project. Examples using AutoFixture

---
+++ XUnitTestTraining/XUnitTestTraining/Tests/AutoFixture/CustomAttributes/CreateManyDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+
+namespace XUnitTestTraining.Tests.AutoFixture.CustomAttributes
+{
+    public class CreateManyDataAttribute : AutoDataAttribute
+    {
+        public CreateManyDataAttribute() : base(() =>
+        {
+            Fixture fixture = new Fixture
+            {
+                RepeatCount = 10
+            };
+            return fixture;
+        }) { }
+    }
+}
